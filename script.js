75581ee12c69acfd243dd8e133e2d462ec8eaf84
Manish kumar <169014705+manishkumar2543@users.noreply.github.com>
2025-09-01 11:13:53 +0530

    scrub
    scrub animation topic

--- script.js
+++ script.js
@@ -1,88 +1,77 @@
-// gsap.to(".box",{
-//     x:1200,
-//     duration:3,
-//     rotate:360,
-//     delay:1,
-//     backgroundColor:"yellow",
-    
-// })
-// gsap.from(".box2",{
-//     x:1000,
-//     duration:3,
-//     translateY:200,
-//     delay:1,
-//     backgroundColor:"pink",
-//     opacity:0,
-
-// })
-
-// from: -end point 
-// to :- start point
-
-
-// gsap.from("h1",{
-//     opacity:0,
+// gsap.from(".page1 .box",{
+//     scale:0.2,
 //     duration:1,
+//     delay:1,
+// })
+// gsap.from(".page2 .box",{
+//     scale:0.2,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     // sortcut ka tarika
+//     // scrollTrigger:".page2 .box"
+//     // deeply dekhe te hai
+//     scrollTrigger:{
+//         trigger:".page2 .box",
+//         scroll:"body",
+//         markers:true,
+//         start:"top 60%",
+//     }
+// })
+
+// gsap.from(".page1 h1",{
+//     y:1,
+//     scale:"0.3",
+//     duration:2,
+//     delay:1,
+//     opacity:0,
+// })
+
+// gsap.from(".page2 h1",{
+//     x:500,
+//     opacity:0,
+//     scale:0.2,
+//     duration:2,
+//     delay:1,
+//     scrollTrigger:{
+//         trigger:".page2 h1",
+      
+//         scroll:"body",
+//         markers:"true", 
+//         start:"top 60%",
+//         end:"top 30%",
+      
+      
+          
+
+//     }
+// })
+// gsap.from(".page2 h2",{
+//     x:-500,
+//     opacity:0,
+//     scale:0.2,
+//     duration:2,
+   
+//     scrollTrigger:{
+//         trigger:".page2 h2",
+//         scroll:"body",
+//         markers:true,
+//         start:"top 50%",
+
+
+//     }
+// })
+// gsap.from(".page3 h1",{
 //     y:20,
-    
-//     delay:1,
-    
-//     //  ye hmar 1 porperty ko dikhtta hai para pri- stagger 📢
-//     stagger:0.5, 
-
-//     // repeat:-1 ye infinite hai 1 kren ek hi bar chalga 📢
-//     repeat:-1, 
-
-//     // yoyo:true ye aage piche hoga 📢
-//     yoyo:true,        
-// }) 
-
-
-// <!-- Gsap timeline and how to control. ( Topic -3  💡) -->
-
-// let tl=gsap.timeline()
-// tl.to(".box1",{
-//      x:800,
+//     opacity:0,
 //     duration:2,
 //     delay:1,
-//     // repeat:-1,
-//     yoyo:true,
-//    })
+//     scale:0.2,
+//     rotate:360,
+//     scrollTrigger:{
+//         trigger:".page3 h1",
+//         scroll:"body",
+//         marker:"true"
+//     }
 
-//     tl.to(".box2",{
-//      x:800,
-//     duration:2,
-//     delay:1
-//    })
-
-//    tl.to(".box3",{
-//     x:800,
-//     duration:2,
-//     delay:1,
-
-//    })
-
-
-let tl=gsap.timeline()
-tl.from("h2",{
-     y:-20,
-     opacity:0,
-     duration:1,
-     delay:1,
-    
-})
-tl.from("h4",{
-     y:-20,
-     opacity:0,
-     duration:1,
-     
-})
-tl.from("h1",{
-     y:-20,
-     duration:0.5,
-     opacity:0,
-     scale:0.2,
-
-})
-
-
+// })
