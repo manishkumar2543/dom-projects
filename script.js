0c833800da323cc30783b4b1b765a0bd6ad71e56
Manish kumar <169014705+manishkumar2543@users.noreply.github.com>
2025-09-04 16:03:37 +0530

    Mouseenter Mouseleave case
    Mini project

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
-var path="M 10 100 Q 500 100 990 100";
-var finalPath="M 10 100 Q 500 100 990 100";
+var main=document.querySelector('.main');
+var cursor=document.querySelector('.cursor');
+var imgDiv=document.querySelector(`.image`)
 
+main.addEventListener("mousemove",function(dets){
+    gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y,
+        ease: "back.out",
+        duration: 0.5,
+        
+    })
+})
 
-var string=document.querySelector(".string")
-// string.addEventListener("mouseenter",function(){
-//     console.log("clicked")
-// })
-
-// string.addEventListener("mousemove",function(dets){
-//     // console.log(dets)
-//     // console.log(dets.x)
-//     // console.log(dets.y)
-// })
-
-string.addEventListener("mousemove",function(dets){
-    path=`M 10 100 Q ${dets.x} ${dets.y} 990 100`; 
-    gsap.to("svg path",{
-        attr:{d:path},
-        ease:"power3.out",
-        duration:0.3,
+imgDiv.addEventListener("mouseenter",function(){
+    cursor.innerHTML="View More",
+    gsap.to(cursor,{
+        
+        scale:4,
 
     })
-});
-
-string.addEventListener("mouseleave",function(){
-    gsap.to("svg path",{
-        attr:{d:finalPath},
-       ease: "elastic.out(1,0.2)",
-        duration:1.5,
+})
+imgDiv.addEventListener("mouseleave",function(){
+    cursor.innerHTML=""
+    gsap.to(cursor,{
+        scale:1,
     })
 })
